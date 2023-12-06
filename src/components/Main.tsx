606d373c718c0592ab533d1a487a7ee731e68382
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-12-06 22:22:17 +0100

    Create a hero section

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,9 @@ import './Main.css'
 export default function Main() {
   return (
     <main>
-
+      <div className="hero-content">
+        <p className="hero-title">IMMERSIVE EXPERIENCES THAT DELIVER</p>
+      </div>
     </main>
   )
 }
