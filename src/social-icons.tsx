7f0a00b0670e0ccb13b996995a84dfd85ac04012
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-12-08 16:28:43 +0100

    Move social icons to public and create a module to import

---
+++ src/social-icons.tsx
@@ -0,0 +1,18 @@
+export default [
+  {
+    desc: 'Facebook',
+    src: './images/icons/icon-facebook.svg'
+  },
+  {
+    desc: 'Twitter',
+    src: './images/icons/icon-twitter.svg'
+  },
+  {
+    desc: 'Pinterest',
+    src: './images/icons/icon-pinterest.svg'
+  },
+  {
+    desc: 'Instagram',
+    src: './images/icons/icon-instagram.svg'
+  }
+]
