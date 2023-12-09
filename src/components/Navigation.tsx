294059c94e7e1fc1702ceb885c184edf44343374
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-12-09 13:26:00 +0100

    Close mobile nav when the viewport width is large

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import useMediaQuery from "../hooks/useMediaQuery"
 import hamburger from '../assets/icon-hamburger.svg'
 import close from '../assets/icon-close.svg'
 import './Navigation.css'
 
 export default function Navigation() {
+  const isLargeScreen = useMediaQuery('(min-width: 768px)')
   const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  // Close mobile navigation if the viewport width is large
+  if (isLargeScreen && isOpen) {
+    setIsOpen(false)
+  }
 
   function handleMenuClick(): void {
     setIsOpen(prevIsOpen => !prevIsOpen)
